1ea18a65624f72c2e412c96459fc74312b90d916
Kai <kwiddeson4@gmail.com>
2022-10-06 22:28:28 +0100

    add header typewriter

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -4,15 +4,29 @@ import ProfileCircle from './ProfileCircle';
 import CircleRipple from './CircleRipple';
 import FootButton from './FootButton';
 
+import '../App.scss';
+
+import Typewriter from 'typewriter-effect';
+
+import {headliners} from '../contents'
+
 function MainHeader() {
     return (
         <header className="App-header">
             {/* <CircleRipple />  */}
             <ProfileCircle profile_img={profile_pic} />
-            <br /> <br /> <br />
-            <div>
+            <div id="name-header">
                 Kai Widdeson
             </div>
+
+            <Typewriter
+                options={{
+                    strings: headliners,
+                    cursor: ">",
+                    autoStart: true,
+                    loop: true,
+                    }}
+            />
             
             <FootButton />
         </header>
---
+++ src/contents.js
@@ -0,0 +1,7 @@
+export const headliners = 
+    ["<1st Class MEng Durham Computer Scientist /", 
+    "<100km Ultramarathon Runner /",
+    "<AICCSA 2022 Conference Academic /",
+    "<Self-Published Fiction Author /",
+    "<Newsletter Creator /"
+    ]
