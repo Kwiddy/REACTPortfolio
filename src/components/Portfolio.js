d2f7e8f91ec4c0ba3547f5933e031a0cfd70cd4b
Kai <kwiddeson4@gmail.com>
2022-10-07 17:16:55 +0100

    add portfolio framework

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,27 @@
 import Card from "./Card"
+import { portfolios } from "../contents";
+
+
+const PortfolioContent = () => {
+    const portfolioItems = portfolios.map((quote) => 
+    <tr>
+        <td> img </td>
+        <td> <b> {quote[0] + " - " + quote[1]} </b> <br /> <i>{"'" + quote[2] + "'"}</i> </td>
+    </tr>
+    );
+    return(
+        <table id="contact-table">
+            <tbody>
+                {portfolioItems}
+            </tbody>
+        </table>
+    );
+}
 
 function Portfolio() {
     return(
         <div className="portfolio">
-            <Card title="Portfolio" content="Some stuff"/>
+            <Card title="Portfolio" content={<PortfolioContent />}/>
         </div>
     )
 }
--- src/contents.js
+++ src/contents.js
@@ -32,3 +32,10 @@ export const testimonials =
         ["Krishna Vinjamuri", "Barclays CTO of Cards & Payments and Connected Platforms", "Excellent work in defining and implementing a number of improvements to the Jira model and generating F2B automated reports - releasing capacity for the teams. Thank you Kai for your outstanding contribution in delivering these improvements for the team"],
         ["Tung Nhon Du", "Barclays Lead UI Developer", "Kai is refreshingly curious about technology and is always keen to learn new approaches. I am sure his positive attitude will be a great advantage in his next engagements. He can be extremely proud of his achievements at Barclays. I'd like to thank Kai for his great effort and wish him all the best for the future."],
     ]
+
+export const portfolios =
+    [
+        ["a", "b", "c"],
+        ["d", "e", "f"],
+        ["g", "h", "i"],
+    ]
