93b8d8d5314ff80e09513b796fab7e694d54eb56
Kai <kwiddeson4@gmail.com>
2022-10-07 20:07:59 +0100

    add about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,29 @@
 import Card from "./Card"
 
+const About = () => {
+    return(
+        <div>
+            <ul>
+                <li> <b>Job: </b> Software Developer 💻 / Front-End Engineer 👨‍💻 </li>
+                <li> <b> Qualifications: </b> 1st Class MEng Computer Science @ Durham University 🏰</li>
+                <li> <b> Field of Interest: </b> Front-end development, Data analytics, Machine learning, Recommender systems, and Software development</li>
+            </ul>
+            <div>
+                I have many interests away from the screens:
+                <ul>
+                    <li> 100km Ultramarathon Runner 🏃‍♂️</li>
+                    <li> Self-Published Fiction Author <a href="#">(Pss... click here)</a> 📖📚 and Newsletter writer 🧾</li>
+                    <li> (Barely) Surfing 🏄‍♂️</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
 function AboutMe() {
     return(
         <div className="aboutme">
-            <Card title="About Me" content="Some stuff"/>
+            <Card title="About Me" content={<About/>}/>
         </div>
     )
 }
