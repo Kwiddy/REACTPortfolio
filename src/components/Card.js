cf5a671704f4a17fc462eee143a5a0734e1957d4
Kai <kwiddeson4@gmail.com>
2022-10-07 16:18:56 +0100

    change color styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,24 @@
+import { AnimatePresence } from "framer-motion";
+import {motion} from 'framer-motion';
+
 function Card(props) {
     return(
-        <div className="card">
-            <div className="card-title">
-                {props.title}
+        <AnimatePresence>
+            <motion.div
+                key="modalf"
+                initial={{ x: 300, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: -300, opacity: 0 }}
+            />
+            <div className="card">
+                <div className="card-title">
+                    {props.title}
+                </div>
+                <div className="card-content">
+                    {props.content}
+                </div>
             </div>
-            <div className="card-content">
-                {props.content}
-            </div>
-        </div>
+        </AnimatePresence>
     );
 }
 
